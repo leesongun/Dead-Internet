7aeb6934417dbf7517834fbb74e959526ed41a9b
Songun Lee <leesongun@naver.com>
2024-04-24 16:58:26 +0000

    use Groq instead of local

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from ReaperEngine import *
 app = flask.Flask(__name__)
 engine = ReaperEngine()
 
-@app.route("/", defaults={'path': ''})
-@app.route('/<path:path>')
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
 def index(path):
     # Handle search and no seach
     query = flask.request.args.get("query")
@@ -17,11 +18,12 @@ def index(path):
         return engine.get_search(query)
     if path == "_export":
         return engine.export_internet()
-    
+
     # Generate the page
     parsed_path = urlparse("http://" + path)
     generated_page = engine.get_page(parsed_path.netloc, path=parsed_path.path)
     return generated_page
+
 
 if __name__ == "__main__":
     app.run()
