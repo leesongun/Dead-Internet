e0ffa899150afb1f76748466cf5e78b868605209
Songun Lee <leesongun@naver.com>
2024-04-25 06:12:10 +0000

    add starting prompt to reduce decline;

--- ReaperEngine.py
+++ ReaperEngine.py
@@ -9,8 +9,8 @@ class ReaperEngine:
         self.client = Groq(
             api_key=os.environ.get("API_KEY"),
         )
-        self.internet_db = dict()
-        self.temperature = 0.2
+        self.cache = dict()
+        self.temperature = 0  # 0.2
         self.max_tokens = 4096
         self.model = "llama3-70b-8192"
         self.system_prompt = "You are an expert in creating realistic webpages. You do not create sample pages, instead you create webpages that are completely realistic and look as if they really existed on the web. You do not respond with anything but HTML, starting your messages with <!DOCTYPE html> and ending them with </html>. If a requested page is not a HTML document, for example a CSS or Javascript file, write that language instead of writing any HTML. If the requested page is instead an image file or other non-text resource, attempt to generate an appropriate resource for it instead of writing any HTML. You use very little to no images at all in your HTML, CSS or JS."
@@ -36,7 +36,7 @@ class ReaperEngine:
     def get_page(self, url, path, query=None):
         # Return already generated page if already generated page
         try:
-            return self.internet_db[url][path]
+            return self.cache[url][path]
         except:
             pass
 
@@ -45,7 +45,7 @@ class ReaperEngine:
         # TODO: I wanna add all other pages to the prompt so the next pages generated resemble them, but since Llama 3 is only 8k context I hesitate to do so
 
         # Add other pages to the prompt if they exist
-        if url in self.internet_db and len(self.internet_db[url]) > 1:
+        if url in self.cache and len(self.cache[url]) > 1:
             pass
 
         # Generate the page
@@ -53,17 +53,18 @@ class ReaperEngine:
             messages=[
                 {"role": "system", "content": self.system_prompt},
                 {"role": "user", "content": prompt},
+                {"role": "assistant", "content": self.start_prompt},
             ],
-            model=self.model,  # What a great model, works near perfectly with this, shame its only got 8k context (does Ollama even set it to that by default?)
+            model=self.model,
             temperature=self.temperature,
             max_tokens=self.max_tokens,
         )
 
         # Add the page to the database
         generated_page = generated_page_completion.choices[0].message.content
-        if not url in self.internet_db:
-            self.internet_db[url] = dict()
-        self.internet_db[url][path] = self._sanitize_links(generated_page)
+        if not url in self.cache:
+            self.cache[url] = dict()
+        self.cache[url][path] = self._sanitize_links(generated_page)
 
         open("curpage.html", "w+").write(generated_page)
         return self._sanitize_links(generated_page)
@@ -77,6 +78,7 @@ class ReaperEngine:
                     "role": "user",
                     "content": f"Generate the search results page for a ficticious search engine where the search query is '{query}'. Please include at least 10 results to different ficticious websites that relate to the query. DO NOT link to any real websites, every link should lead to a ficticious website. Feel free to add a bit of CSS to make the page look nice. Each search result will link to its own unique website that has nothing to do with the search engine. Make sure each ficticious website has a unique and somewhat creative URL. Don't mention that the results are ficticious.",
                 },
+                {"role": "assistant", "content": self.start_prompt},
             ],
             model=self.model,
             temperature=self.temperature,
@@ -86,4 +88,4 @@ class ReaperEngine:
         return self._sanitize_links(search_page_completion.choices[0].message.content)
 
     def export_internet(self, filename="internet.json"):
-        json.dump(self.internet_db, open(filename, "w+"))
+        json.dump(self.cache, open(filename, "w+"))
